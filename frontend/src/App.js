bb5952c3d9e02da45b9afd8f655b18b388e6c32a
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-03-30 02:54:49 +0200

    feat: login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
+//import { useEffect } from 'react';
+//import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+
+import Homepage from './pages/Homepage/Homepage';
+import Login from './pages/Login/Login';
+
 import './App.css';
 
-import { useEffect } from 'react';
-import axios from 'axios';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
 
 function App() {
 
-  useEffect(() => {
+  /*useEffect(() => {
     axios.get('http://localhost:8081/')
     .then((res) => {
         console.log(res.data)
     })
-  }, []);
+  }, []);*/
 
   return (
     <div className="App">
         <Routes>
-            <Route path='/' element={<Homepage />} />
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Homepage />} />
         </Routes>
     </div>
   );
