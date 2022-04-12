114c598e24ac310d2d220c4a69f68a648d1d6b6e
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-04-12 02:18:40 +0200

    feat: login and registration

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,6 +1,13 @@
-
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Homepage(props) {
+    const user = useSelector((state) => state.user.value);
+
+    useEffect(() => {
+        console.log(user)
+    }, []);
+
     return (
         <div>
             Homepage
